67c0419a94819816d4a1c6a630780c70e6b552b5
Chris Karani <chrisbkarani@gmail.com>
2017-11-17 10:08:43 +0300

    Initial Commit

---
+++ ChristamasBundleAppFiverr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChristamasBundleAppFiverr
+//
+//  Created by Christopher Brandon Karani on 17/11/2017.
+//  Copyright © 2017 Christopher Brandon Karani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
